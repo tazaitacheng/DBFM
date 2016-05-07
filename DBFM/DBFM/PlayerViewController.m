b3454ff22103dbf8c416b9ff4cc4e6332c57cac8
hejian <377933961@qq.com>
2016-05-07 15:18:26 +0800

    创建了PlayController

---
+++ DBFM/DBFM/PlayerViewController.m
@@ -0,0 +1,55 @@
+//
+//  PlayerViewController.m
+//  DBFM
+//
+//  Created by lkjy on 16/5/7.
+//  Copyright © 2016年 何健. All rights reserved.
+//
+
+#import "PlayerViewController.h"
+#import "PlayerController.h"
+#import "ChannelInfo.h"
+
+#import <QuartzCore/QuartzCore.h>
+#import <UIKit+AFNetworking.h>
+#import <UIImageView+WebCache.h>
+#import <Masonry/Masonry.h>
+
+#import <AFNetworking/AFNetworking.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+#import "NetworkManager.h"
+#import "AppDelegate.h"
+#import "SongInfo.h"
+#import "ProtocolClass.h"
+
+
+
+
+@interface PlayerViewController ()
+
+@end
+
+@implementation PlayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
