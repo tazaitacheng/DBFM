d24e3e3a358cf673220b1f734a5ebafd46abedd5
caroline <747833539@qq.com>
2016-05-07 15:47:17 +0800

    Channels cell

---
+++ DBFM/DBFM/ChannelsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ChannelsTableViewCell.h
+//  DBFM
+//
+//  Created by lkjy on 16/5/7.
+//  Copyright © 2016年 何健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChannelsTableViewCell : UITableViewCell
+
+@end
---
+++ DBFM/DBFM/ChannelsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ChannelsTableViewCell.m
+//  DBFM
+//
+//  Created by lkjy on 16/5/7.
+//  Copyright © 2016年 何健. All rights reserved.
+//
+
+#import "ChannelsTableViewCell.h"
+
+@implementation ChannelsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
