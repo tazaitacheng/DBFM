01348da8ceedee6b12ccbf9340c7d1e99d3a1f2b
humengchen <1533592021@qq.com>
2016-05-07 15:08:49 +0800

    SidebarController

---
+++ DBFM/DBFM/SidebarController.h
@@ -0,0 +1,14 @@
+//
+//  SidebarController.h
+//  DBFM
+//
+//  Created by ljky on 16/5/7.
+//  Copyright © 2016年 何健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CDSideBarController.h"
+
+@interface SidebarController : UITabBarController<CDSideBarControllerDelegate>
+
+@end
